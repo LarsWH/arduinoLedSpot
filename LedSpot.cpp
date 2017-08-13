8b93e25c89da69fbf8b5f5692deea71c59302809
Lars Weje Hangstrup <lars@hangstrup.dk>
2017-08-13 18:48:01 +0200

    Simplified

--- LedSpot.cpp
+++ LedSpot.cpp
@@ -1,9 +1,12 @@
-#include "LedSpot.h"
 #include <EEPROM.h>
 #define EEPROM_ADDR_OF_VAL 0
 #define PWM_PIN  3
+#define PUSH_BUTTON_PIN 5
 
-
+#define VAL_MIN 64
+#define VAL_MAX 255
+#define MAX_PWM 255
+#define STEPS (VAL_MAX-VAL_MIN+1)
 #define RAMP_TIME 4000
 #define INCREMENT_TIME (RAMP_TIME/STEPS)
 #define TICK_TIME 10
@@ -11,77 +14,38 @@
 #define PRELL_TICKS (PRELL_TIME/TICK_TIME)
 #define INCREMENT_TICKS (INCREMENT_TIME/TICK_TIME)
 
-#define USE_FLOAT
-#ifdef USE_FLOAT
-	#define VAL_MIN 64
-	#define VAL_MAX 255
-	#define MAX_PWM 255
-	#define STEPS (VAL_MAX-VAL_MIN+1)
-	float max = 3.9961; //6.3413;
-#else
-	#define VAL_MIN 0
-	#define VAL_MAX 10
-	#define STEPS (VAL_MAX + 1)
-	int pwm[STEPS] = {
-		1, //1.7404
-		2,
-		3,
-		5,
-		9,
-		15,
-		27,
-		48,
-		84,
-		146,
-		255 };
+typedef enum Event {
+	EVENT_NOTHING,
+	EVENT_KEY_PRESS,
+	EVENT_KEY_RELEASE,
+	EVENT_TIMER_TICK
+} EventType;
 
-#endif
+typedef enum KeyState {
+	KEY_STATE_RELEASED,
+	KEY_STATE_BEING_PRESSED,
+	KEY_STATE_PRESSED,
+	KEY_STATE_BEING_RELEASED,
+} KeyStateType;
 
+typedef enum RegulateState {
+	IDLE,
+	INCREMENT,
+	DECREMENT
+} RegulateStateType;
+
+
+//---------- Global variables ----------
+float max = 3.9961; //  max*max*max*max = 255
 int val = VAL_MIN;
+bool isIncrementing = true;
 
 
+//---------- Prototypes ----------
+EventType keyDetect();
+void rampUp(int value);
+void regulate(EventType event);
 
-#define PUSH_BUTTON_PIN 5
-
-void writeLed() {
-#ifdef USE_FLOAT
-
-	Serial.print("  val:");
-	Serial.print(val);
-
-	float scaled = ((float)val * max) / (float)VAL_MAX;
-
-	Serial.print("  scaled:");
-	Serial.print(scaled);
-
-
-	float pwmVal = scaled * scaled * scaled * scaled;
-
-	Serial.print("  pwmVal:");
-	Serial.print(pwmVal);
-
-	int pwmInt = (int)pwmVal;
-	if (pwmInt > MAX_PWM) {
-		pwmInt = MAX_PWM;
-	}
-#else
-	int pwmInt = pwm[val];
-#endif
-
-	Serial.print(" PWM:");
-	Serial.println(pwmInt);
-
-	analogWrite(PWM_PIN, pwmInt);
-}
-
-void rampUp(int value) {
-	int valuesToRamp = value - VAL_MIN;
-	for (int i = VAL_MIN; i <= value; i++) {
-		val = i;
-		writeLed();
-		delay(INCREMENT_TIME);
-	}
-}
 
 void setup()
 {
@@ -106,6 +70,51 @@ void setup()
 	rampUp(value);
 }
 
+void loop()
+{
+	EventType event = keyDetect();
+	if (event == EVENT_NOTHING) {
+		regulate(EVENT_TIMER_TICK);
+	} else {
+		regulate(event);
+	}
+
+	delay(TICK_TIME);
+}
+
+
+void writeLed() {
+	Serial.print("  val:");
+	Serial.print(val);
+
+	float scaled = ((float)val * max) / (float)VAL_MAX;
+	Serial.print("  scaled:");
+	Serial.print(scaled);
+
+	float pwmVal = scaled * scaled * scaled * scaled;
+	Serial.print("  pwmVal:");
+	Serial.print(pwmVal);
+
+	int pwmInt = (int)pwmVal;
+	if (pwmInt > MAX_PWM) {
+		pwmInt = MAX_PWM;
+	}
+	Serial.print(" PWM:");
+	Serial.println(pwmInt);
+
+	analogWrite(PWM_PIN, pwmInt);
+}
+
+
+void rampUp(int value) {
+	int valuesToRamp = value - VAL_MIN;
+	for (int i = VAL_MIN; i <= value; i++) {
+		val = i;
+		writeLed();
+		delay(INCREMENT_TIME);
+	}
+}
+
 bool incrementLed() {
 	bool incremented = false;
 	if (val < VAL_MAX) {
@@ -128,11 +137,6 @@ bool decrementLed() {
 }
 
 
-
-bool isIncrementing = true;
-
-
-
 void toggleDirection() {
 	isIncrementing = !isIncrementing;
 }
@@ -141,7 +145,6 @@ void toggleDirection() {
 void regulate(EventType event) {
 	static RegulateStateType state = IDLE;
 	static int incrementTicks = INCREMENT_TICKS;
-	char *pString = "Woops";
 
 	switch (event) 	{
 		case EVENT_KEY_RELEASE:
@@ -154,7 +157,6 @@ void regulate(EventType event) {
 
 	switch (state) {
 		case IDLE:
-			pString = "IDLE";
 			switch (event) {
 				case EVENT_KEY_PRESS:
 					if (isIncrementing) {
@@ -170,7 +172,6 @@ void regulate(EventType event) {
 			break;
 
 		case INCREMENT:
-			pString = "INCREMENT";
 			switch (event) {
 				case EVENT_TIMER_TICK:
 					if (!incrementTicks--) {
@@ -188,7 +189,6 @@ void regulate(EventType event) {
 			break;
 
 		case DECREMENT:
-			pString = "DECREMENT";
 			switch (event) {
 				case EVENT_TIMER_TICK:
 					if (!incrementTicks--) {
@@ -207,25 +207,7 @@ void regulate(EventType event) {
 
 	}
 
-/*	static int x = 0;
-	Serial.print(x++);
-	Serial.print(" State: ");
-	Serial.println(pString); */
 }
-
-void loop()
-{
-	EventType event = keyDetect();
-	if (event == EVENT_NOTHING) {
-		regulate(EVENT_TIMER_TICK);
-	} else {
-		regulate(event);
-	}
-
-	delay(TICK_TIME);
-}
-
-
 
 EventType keyDetect() {
 	EventType event = EVENT_NOTHING;
@@ -234,49 +216,45 @@ EventType keyDetect() {
 	int buttonDown = !digitalRead(PUSH_BUTTON_PIN);
 
 	switch (keyState) {
-	case KEY_STATE_RELEASED:
-		if (buttonDown) {
-			keyState = KEY_STATE_BEING_PRESSED;
-			prellDetects = PRELL_TICKS;
-		}
-		break;
-	case KEY_STATE_BEING_PRESSED:
-		if (buttonDown) {
-			prellDetects--;
-		}
-		else {
-			keyState = KEY_STATE_RELEASED;
-		}
+		case KEY_STATE_RELEASED:
+			if (buttonDown) {
+				keyState = KEY_STATE_BEING_PRESSED;
+				prellDetects = PRELL_TICKS;
+			}
+			break;
+		case KEY_STATE_BEING_PRESSED:
+			if (buttonDown) {
+				prellDetects--;
+			}
+			else {
+				keyState = KEY_STATE_RELEASED;
+			}
 
-		if (!prellDetects) {
-			keyState = KEY_STATE_PRESSED;
-			event = EVENT_KEY_PRESS;
-			Serial.println("---- EVENT: PRESSED");
-		}
-		break;
-	case KEY_STATE_PRESSED:
-		if (!buttonDown) {
-			keyState = KEY_STATE_BEING_RELEASED;
-			prellDetects = PRELL_TICKS;
-		}
-		break;
-	case KEY_STATE_BEING_RELEASED:
-		if (!buttonDown) {
-			prellDetects--;
-		}
-		else {
-			keyState = KEY_STATE_PRESSED;
-		}
+			if (!prellDetects) {
+				keyState = KEY_STATE_PRESSED;
+				event = EVENT_KEY_PRESS;
+			}
+			break;
+		case KEY_STATE_PRESSED:
+			if (!buttonDown) {
+				keyState = KEY_STATE_BEING_RELEASED;
+				prellDetects = PRELL_TICKS;
+			}
+			break;
+		case KEY_STATE_BEING_RELEASED:
+			if (!buttonDown) {
+				prellDetects--;
+			}
+			else {
+				keyState = KEY_STATE_PRESSED;
+			}
 
-		if (!prellDetects) {
-			keyState = KEY_STATE_RELEASED;
-			event = EVENT_KEY_RELEASE;
-			Serial.println("---- EVENT: RELEASED");
-		}
-		break;
-
+			if (!prellDetects) {
+				keyState = KEY_STATE_RELEASED;
+				event = EVENT_KEY_RELEASE;
+			}
+			break;
 	}
-
 
 	return event;
 }
